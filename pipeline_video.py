43952c320008a228f639d3f401badc257a4d4d78
gleyframework-ctrl <gleyframework@gmail.com>
2026-09-09 23:01:33 +0200

    Update pipeline_video.py

--- pipeline_video.py
+++ pipeline_video.py
@@ -7,10 +7,10 @@ from pypdf import PdfReader
 from PIL import Image
 import io
 
-# 🔑 API KEYS - Read from Environment Variables
+# 🔑 API KEYS - Read from Environment Variables (CORRECT WAY!)
 NVIDIA_API_KEY = os.environ.get("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")
-CARTESIA_API_KEY = os.environ.get("aee2a343-ab30-430a-b50d-34eaec3dfba6")
-CARTESIA_VOICE_ID = os.environ.get("sk_car_j1AAGoyVXQF5pZ7W4zvydX")
+CARTESIA_API_KEY = os.environ.get("sk_car_j1AAGoyVXQF5pZ7W4zvydX")
+CARTESIA_VOICE_ID = os.environ.get("aee2a343-ab30-430a-b50d-34eaec3dfba6")
 
 if not NVIDIA_API_KEY or not CARTESIA_API_KEY:
     raise ValueError("Missing API keys! Set NVIDIA_API_KEY and CARTESIA_API_KEY in Railway variables.")
